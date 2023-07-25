76569498ca377922676e76fcf8555438cb4618c4
Rain <eunbkang@gmail.com>
2023-07-26 00:06:26 +0900

    added pull down button menu

--- MoodCounter/InputViewController.swift
+++ MoodCounter/InputViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class InputViewController: UIViewController {
-
+    @IBOutlet var moodButtons: [UIButton]!
+    
     var countedValue: [Int] = [0, 0, 0, 0, 0]
     
     let moodNames = Mood.allCases
@@ -16,13 +17,44 @@ class InputViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configMoodButtons()
     }
 
     @IBAction func tappedMoodButton(_ sender: UIButton) {
         let index = sender.tag - 1
-        countedValue[index] += 1
+        addScore(index: index, score: 1)
+    }
+    
+    func configMoodButtons() {
+        for button in moodButtons {
+            configMoodButtonMenu(button)
+        }
+    }
+    
+    func configMoodButtonMenu(_ button: UIButton) {
+        let index = button.tag - 1
+        let score1 = UIAction(title: "+1점", handler: { _ in
+            self.addScore(index: index, score: 1)
+        })
+        let score5 = UIAction(title: "+5점", handler: { _ in
+            self.addScore(index: index, score: 5)
+        })
+        let score10 = UIAction(title: "+10점", handler: { _ in
+            self.addScore(index: index, score: 10)
+        })
+        let reset = UIAction(title: "초기화", handler: { _ in
+            self.countedValue[index] = 0
+            print("\(self.moodNames[index].rawValue) \(self.countedValue[index])점으로 초기화")
+            self.printAllScores()
+        })
         
-        print("\(moodNames[index].rawValue) 1점 추가, 총 \(countedValue[index])점")
+        let buttonMenu = UIMenu(children: [score1, score5, score10, reset])
+        button.menu = buttonMenu
+    }
+    
+    func addScore(index: Int, score: Int) {
+        countedValue[index] += score
+        print("\(moodNames[index].rawValue) \(score)점 추가하여 총 \(countedValue[index])점")
         printAllScores()
     }
     
